a5421be421bbee0a0bd068f54ec7792f303ede50
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-06-23 11:25:08 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 388 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void){
+    int a,b,c;
+    scanf("%d",&c);
+    for(int i=0;i<c;i++){
+        scanf("%d\n%d",&a,&b);
+        printf("%d\n",a+b);
+    }
+    return 0;
+}
