4dc886fb3105b0685f7a8b1b24c4175b23511af3
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-06-27 14:36:05 +0900

    [Bronze V] Title: 아스키 코드, Time: 60 ms, Memory: 5372 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Text;
+
+class Program{
+    static void Main(){
+ StringBuilder sb = new StringBuilder();
+            int a=char.Parse(Console.ReadLine());
+            sb.Append(a+"\n");
+            Console.WriteLine(sb.ToString());}
+}
