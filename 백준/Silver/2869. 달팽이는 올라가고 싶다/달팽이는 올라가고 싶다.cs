62f0d6021c9d211ed86b88f625c724083ab21091
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-06-28 15:24:21 +0900

    [Silver V] Title: 달팽이는 올라가고 싶다, Time: 76 ms, Memory: 5820 KB -BaekjoonHub

---
+++ 백준/Silver/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+    {
+
+        static void Main(string[] agrs)
+        {
+            string[] s = Console.ReadLine().Split();
+            int A = int.Parse(s[0]);
+            int B = int.Parse(s[1]);
+            int V = int.Parse(s[2]);
+            int day = 0;
+            int H = 0;
+            int count = 0;
+            day = V / (A - B);
+            while ((V - (day - count-1) * (A - B)) <= A)
+            {
+                count++;
+            }
+                
+            Console.WriteLine(day-count+1);
+        }
+    }
