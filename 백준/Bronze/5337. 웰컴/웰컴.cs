fdb37979e22bd90340a92662a5ab0c48f8b12d88
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-04 13:47:48 +0900

    [Bronze V] Title: 웰컴, Time: 56 ms, Memory: 5312 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(".  .   .");
+        Console.WriteLine("|  | _ | _. _ ._ _  _");
+        Console.WriteLine("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
