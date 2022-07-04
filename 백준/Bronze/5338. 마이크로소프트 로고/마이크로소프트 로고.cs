441474b68dfd5b1d251b8aee45220a58ef2c0efa
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-04 13:51:03 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 60 ms, Memory: 5300 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("       _.-;;-._");
+        Console.WriteLine("'-..-'|   ||   |");
+        Console.WriteLine("'-..-'|_.-;;-._|");
+        Console.WriteLine("'-..-'|   ||   |");
+        Console.WriteLine("'-..-'|_.-''-._|");
+    }
+}
