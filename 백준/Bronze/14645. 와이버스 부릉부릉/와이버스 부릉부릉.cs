b7d8076a777319ac4ba38e46e38180bf54796c47
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-04 14:31:42 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 64 ms, Memory: 5432 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] N = Console.ReadLine().Split();
+
+        for(int i = 0; i < int.Parse(N[0]); i++)
+        {
+            Console.ReadLine();
+        }
+        Console.WriteLine("비와이");
+    }
+}
