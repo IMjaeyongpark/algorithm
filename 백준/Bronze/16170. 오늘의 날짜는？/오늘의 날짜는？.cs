7a9e9d94eb8253898573e309bd8fadab153e124d
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-04 14:47:19 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 64 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine(DateTime.Now.Year + "\n" + $"{DateTime.Now.Month:D2}" + "\n" + $"{DateTime.Now.Day:D2}");
+    }
+}
