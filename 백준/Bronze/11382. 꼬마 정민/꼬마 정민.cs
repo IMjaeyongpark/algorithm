4c30ab9a4a0f6c73c456c3abaad828b49f9a0b9c
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-04 14:24:16 +0900

    [Bronze V] Title: 꼬마 정민, Time: 60 ms, Memory: 5464 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program{
+    static void Main(){
+        string[] s =Console.ReadLine().Split();
+        
+        Console.WriteLine(long.Parse(s[0])+long.Parse(s[1])+long.Parse(s[2]));
+    }
+}
