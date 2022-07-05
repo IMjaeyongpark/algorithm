c6b8b429638164e82c7f8d9de9b5c13d2b5d6346
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-05 09:50:35 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 68 ms, Memory: 5340 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        Console.WriteLine(":fan::fan::fan:");
+        Console.WriteLine(":fan::" + s + "::fan:");
+        Console.WriteLine(":fan::fan::fan:");
+    }
+}
