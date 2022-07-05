3cbf33139889478f5a0b1071f0f4db56c7c979b6
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-05 11:00:39 +0900

    [Bronze IV] Title: 운동장 한 바퀴, Time: 60 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/16486. 운동장 한 바퀴/운동장 한 바퀴.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine(a*2 + (double)b*3.141592*2.0);
+    }
+}
