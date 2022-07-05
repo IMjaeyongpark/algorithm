100e6fb982366b68a01cfd52c34ad576d9968d0c
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-05 15:17:09 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 68 ms, Memory: 5708 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+
+        StringBuilder sb = new StringBuilder();
+        int N = int.Parse(Console.ReadLine());
+        for(int i = 0; i < N; i++)
+        {
+            for(int j=0; j < i; j++)
+            {
+                sb.Append(' ');
+            }
+            for(int j = 0; j < 2*(N-i)-1; j++)
+            {
+                sb.Append('*');
+            }
+            sb.Append('\n');
+        }
+        for (int i = 1; i < N; i++)
+        {
+            for(int j =1; j < N - i; j++)
+            {
+                sb.Append(' ');
+            }
+            for (int j = 0; j < 2 * (i + 1) - 1; j++)
+            {
+                sb.Append('*');
+            }
+            sb.Append('\n');
+        }
+        Console.WriteLine(sb);
+    }
+}
