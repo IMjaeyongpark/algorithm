d5b1b9c5ec44ba02fb3cb3eee1fa5675a7c4ac29
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-05 10:04:00 +0900

    [Bronze IV] Title: 연세대학교, Time: 60 ms, Memory: 5456 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        if (N == 0) Console.WriteLine("YONSEI");
+        else Console.WriteLine("Leading the Way to the Future");
+    }
+}
