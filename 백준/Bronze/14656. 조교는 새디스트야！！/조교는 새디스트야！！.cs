1dbea9dd311be08ebdc8d63943107420a05e30bb
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-07 10:39:32 +0900

    [Bronze III] Title: 조교는 새디스트야!!, Time: 68 ms, Memory: 7072 KB -BaekjoonHub

---
+++ 백준/Bronze/14656. 조교는 새디스트야！！/조교는 새디스트야！！.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        string[] s = Console.ReadLine().Split();
+        int count = 0;
+        for(int i = 0; i < N; i++)
+        {
+            if (i + 1 != int.Parse(s[i])) count++;
+        }
+        Console.WriteLine(count);
+    }
+}
