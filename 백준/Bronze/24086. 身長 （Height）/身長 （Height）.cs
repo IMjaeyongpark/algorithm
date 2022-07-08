56dced29b5cd50b4a4dd51e0321af81423d0aa6f
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-08 10:24:13 +0900

    [Bronze V] Title: 身長 (Height), Time: 60 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/24086. 身長 （Height）/身長 （Height）.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int M = int.Parse(Console.ReadLine());
+        Console.WriteLine(M - N);
+    }
+}
