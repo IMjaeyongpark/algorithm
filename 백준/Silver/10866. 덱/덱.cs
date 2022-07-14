66cfad23c7f45fa40247f7aae716f3de329ccf93
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-14 10:50:08 +0900

    [Silver IV] Title: 덱, Time: 88 ms, Memory: 8084 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        StringBuilder sb = new StringBuilder();
+        int N = int.Parse(Console.ReadLine());
+        List<int> list = new List<int>();
+        for (int i = 0; i < N; i++)
+        {
+            string[] s = Console.ReadLine().Split();
+            if (s[0] == "push_back")
+            {
+                list.Add(int.Parse(s[1]));
+            }
+            else if (s[0] == "push_front")
+            {
+                list.Insert(0, int.Parse(s[1]));
+            }
+            else if (s[0] == "pop_front")
+            {
+                if (list.Count > 0)
+                {
+                    sb.Append(list[0] + "\n");
+                    list.RemoveAt(0);
+                }
+                else sb.Append(-1 + "\n");
+            }
+            else if (s[0] == "pop_back")
+            {
+                if (list.Count > 0)
+                {
+                    sb.Append(list[list.Count - 1] + "\n");
+                    list.RemoveAt(list.Count - 1);
+                }
+                else sb.Append(-1 + "\n");
+            }
+            else if (s[0] == "size")
+            {
+                sb.Append(list.Count + "\n");
+            }
+            else if (s[0] == "empty")
+            {
+                if (list.Count == 0) sb.Append(1 + "\n");
+                else sb.Append(0 + "\n");
+            }
+            else if (s[0] == "front")
+            {
+                if (list.Count > 0)
+                {
+                    sb.Append(list[0] + "\n");
+                }
+                else sb.Append(-1 + "\n");
+            }
+            else if (s[0] == "back")
+            {
+                if (list.Count > 0)
+                {
+                    sb.Append(list[list.Count - 1] + "\n");
+                }
+                else sb.Append(-1 + "\n");
+            }
+
+        }
+        Console.WriteLine(sb);
+    }
+}
