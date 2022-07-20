69ea134d5cb9d3bb0ef2631e8b8741d4fc0aa9d5
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-20 14:34:36 +0900

    [Silver II] Title: 좌표 압축, Time: 1896 ms, Memory: 167272 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.cs
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cs
@@ -12,14 +12,8 @@ class Program
         int[] data = arr.Distinct().ToArray();
         Array.Sort(data);
         Dictionary<int, int> dic = new Dictionary<int, int>();
-        for(int i = 0; i < data.Length; i++)
-        {
-            dic.Add(data[i], i);
-        }
-        for(int i = 0; i < arr.Length; i++)
-        {
-            sb.Append(dic[arr[i]] + " ");
-        }
+        for (int i = 0; i < data.Length; i++) dic.Add(data[i], i);
+        for (int i = 0; i < arr.Length; i++) sb.Append(dic[arr[i]] + " ");
         Console.WriteLine(sb);
     }
 }
