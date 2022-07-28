ddd7f388aa67e14d2f9e31e0f78a95f6012698c7
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-07-28 13:46:53 +0900

    [Silver II] Title: 마인크래프트, Time: 428 ms, Memory: 19456 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.cs
@@ -0,0 +1,59 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] s = Console.ReadLine().Split();
+        int y = int.Parse(s[0]);
+        int x = int.Parse(s[1]);
+        int block = int.Parse(s[2]);
+        int[,] grand = new int[x, y];
+        int second = int.MaxValue;
+        int H = 0;
+        int max = 0;
+        int min = int.MaxValue;
+        for(int i = 0; i < y; i++)
+        {
+            string[] s1 = Console.ReadLine().Split();
+            for(int j = 0; j < x; j++)
+            {
+                int a = int.Parse(s1[j]);
+                if (a > max) max = a;
+                if (a < min) min = a;
+                grand[j, i] = a;
+            }
+        }
+
+
+        int bcnt = 0;
+        for (int k = min; true; k++)
+        {
+            bcnt = 0;
+            int second2 = 0;
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    if (k > grand[j, i])
+                    {
+                        bcnt += k - grand[j, i];
+                        second2 += k - grand[j, i];
+                    }
+                    else if (k < grand[j, i])
+                    {
+                        second2 += (grand[j, i] - k) * 2;
+                        bcnt -= grand[j, i] - k;
+                    }
+                }
+            }
+            if (bcnt > block) break;
+            if (second >= second2)
+            {
+                second = second2;
+                if (H < k) H = k;
+            }
+        }
+        Console.WriteLine(second + " " + H);
+    }
+}
