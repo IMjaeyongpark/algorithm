12f4ef54b5c6390ba2d5c7dc699f715686e103df
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-08-01 18:23:55 +0900

    [Silver I] Title: 숨바꼭질, Time: 72 ms, Memory: 7028 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cs
@@ -0,0 +1,35 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] s = Console.ReadLine().Split();
+        int N = int.Parse(s[0]);
+        int K = int.Parse(s[1]);
+        int[] visit = new int[100001];
+        Queue<int> q = new Queue<int>();
+        q.Enqueue(N);
+
+        while (N != K && visit[K] == 0)
+        {
+            int x = q.Dequeue();
+            if (x - 1 >= 0 && visit[x - 1] == 0)
+            {
+                visit[x - 1] = visit[x] + 1;
+                q.Enqueue(x - 1);
+            }
+            if (x + 1 < visit.Length && visit[x + 1] == 0)
+            {
+                visit[x + 1] = visit[x] + 1;
+                q.Enqueue(x + 1);
+            }
+            if (2 * x < visit.Length && visit[2 * x] == 0)
+            {
+                visit[2 * x] = visit[x] + 1;
+                q.Enqueue(2 * x);
+            }
+        }
+        Console.WriteLine(visit[K]);
+    }
+}
