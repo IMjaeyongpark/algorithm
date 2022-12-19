78727c7cf1f79f3c74eacc9f664ed23679bede01
IMjaeyongpark <104854450+IMjaeyongpark@users.noreply.github.com>
2022-12-19 15:40:38 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 60 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(arr: IntArray): Double {
+        var answer:Double = 0.0
+        var sum = 0
+        for(num in arr){
+            sum += num
+        }
+        answer = sum.toDouble()/arr.size.toDouble()
+        
+        return answer
+    }
+}
