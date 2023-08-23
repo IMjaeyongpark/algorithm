fcdc0993517b95b8bf4cb15962ce3351581f7883
박재용 <104854450+IMjaeyongpark@users.noreply.github.com>
2023-08-23 15:41:39 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,9 +1,7 @@
 def solution(n):
     cur = 1
-    while cur<=n:
-        if cur%3==0:
-            n +=1
-        elif '3'in str(cur):
-            n+=1
-        cur+=1
+    while cur <= n:
+        if cur % 3 == 0 or '3' in str(cur):
+            n += 1
+        cur += 1
     return n
