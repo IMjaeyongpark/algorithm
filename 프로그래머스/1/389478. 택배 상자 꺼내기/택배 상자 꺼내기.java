6641512029586b7f93130a7780d93564937133dc
박재용 <104854450+IMjaeyongpark@users.noreply.github.com>
2026-02-15 16:08:50 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.02 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,20 +1,19 @@
 class Solution {
     public int solution(int n, int w, int num) {
         
-        int num_floor = (num - 1) / w;
-        int a = num % w;
-        a = (a == 0) ? w : a;
-        if(num_floor % 2 != 0) a = w - a + 1;
+        int numRow = (num - 1) / w;
+        int col = num % w;
+        col = (col == 0) ? w : col;
+        if(numRow % 2 != 0) col = w - col + 1;
+                
+        int lastRow = (n - 1) / w;
+        int lastCount = n % w;
+        lastCount = (lastCount == 0) ? w : lastCount;
         
+        int answer = lastRow - numRow ;
         
-        int n_floor = (n - 1) / w;
-        int b = n % w;
-        b = (b == 0) ? w : b;
-        
-        int answer = n_floor - num_floor ;
-        
-        if(n_floor % 2 == 0 && b >= a ||
-           n_floor % 2 != 0 && (w - b) < a){
+        if(lastRow % 2 == 0 && lastCount >= col ||
+           lastRow % 2 != 0 && (w - lastCount) < col){
             answer++;
         }
         
